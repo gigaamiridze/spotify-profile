cdfa4e6a577f88061005b052996f3db12e7dc1a4
justtfelix <justttfelix@gmail.com>
2023-07-08 13:23:52 +0400

    Added hover effect for menu item

--- src/components/navbar/Menu.ts
+++ src/components/navbar/Menu.ts
@@ -1,4 +1,4 @@
-import styled from 'styled-components';
+import styled, { css } from 'styled-components';
 
 export const Menu = styled.ul`
   width: 100%;
@@ -8,21 +8,31 @@ export const Menu = styled.ul`
 
 export const MenuItem = styled.li`
   a {
+    ${({ theme }) => css`
+      color: ${theme.colors.lightGrey};
+      fill: ${theme.colors.lightGrey};
+      transition: ${theme.transition};
+    `}
     display: flex;
     flex-direction: column;
     align-items: center;
     row-gap: 7px;
+    font-size: 11px;
     padding: 16px 0;
     border-left: 5px solid transparent;
 
-    svg {
-      fill: ${({ theme }) => theme.colors.lightGrey};
-      width: 20px;
+    &:hover,
+    &:focus {
+      ${({ theme }) => css`
+        fill: ${theme.colors.white};
+        color: ${theme.colors.white};
+        background-color: ${theme.colors.black};
+        border-left-color: ${theme.colors.lightGreen};
+      `}
     }
 
-    span {
-      color: ${({ theme }) => theme.colors.lightGrey};
-      font-size: 11px;
+    svg {
+      width: 20px;
     }
   }
 `;
