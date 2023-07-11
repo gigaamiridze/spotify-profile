0630e2e1611fb08ef943d79449a5b1b15aaad5ac
justtfelix <justttfelix@gmail.com>
2023-07-11 09:56:56 +0400

    Added user's top artists and top tracks fetcher functions

--- src/utils/spotify.ts
+++ src/utils/spotify.ts
@@ -1,5 +1,6 @@
 import axios from 'axios';
 import { requestConfig } from '../config';
+import { TopTracks } from '../pages';
 
 // Get Current User's Profile
 const getUser = () => axios.get('/', { ...requestConfig });
@@ -7,18 +8,40 @@ const getUser = () => axios.get('/', { ...requestConfig });
 // Get User's Followed Artists
 const getFollowing = () => axios.get('/following?type=artist', { ...requestConfig });
 
-// Get a List of Current User's Playlists
+// Get List of Current User's Playlists
 const getPlaylists = () => axios.get('/playlists', { ...requestConfig });
+
+// Get User's Top Artists
+const getTopArtistsShort = () =>
+  axios.get('/top/artists?limit=50&time_range=short_term', { ...requestConfig });
+
+const getTopArtistsMedium = () =>
+  axios.get('/top/artists?limit=50&time_range=medium_term', { ...requestConfig });
+
+const getTopArtistsLong = () =>
+  axios.get('/top/artists?limit=50&time_range=long_term', { ...requestConfig });
+
+// Get User's Top Tracks
+const getTopTracksShort = () =>
+  axios.get('/top/tracks?limit=50&time_range=short_term', { ...requestConfig });
+
+const getTopTracksMedium = () =>
+  axios.get('/top/tracks?limit=50&time_range=medium_term', { ...requestConfig });
+
+const getTopTracksLong = () =>
+  axios.get('/top/tracks?limit=50&time_range=long_term', { ...requestConfig });
 
 export const getUserInfo = () => {
   return (
     axios
-      .all([getUser(), getFollowing(), getPlaylists()])
+      .all([getUser(), getFollowing(), getPlaylists(), getTopArtistsLong(), getTopTracksLong()])
       .then(
-        axios.spread((user, following, playlists) => ({
+        axios.spread((user, following, playlists, topArtists, topTracks) => ({
           user: user.data,
           following: following.data,
           playlists: playlists.data,
+          topArtists: topArtists.data,
+          topTracks: topTracks.data,
         })),
       )
   )
