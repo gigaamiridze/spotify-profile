4042a2794df3882609b613e46ba80a789c719f4d
justtfelix <justttfelix@gmail.com>
2023-07-11 07:40:41 +0400

    User data has become dynamic

--- src/assets/images/index.ts
+++ src/assets/images/index.ts
@@ -1,2 +1,1 @@
 export { default as SpotifyLogo } from './spotify-logo.png';
-export { default as AvatarImg } from './avatar.jpg';
--- src/pages/User.tsx
+++ src/pages/User.tsx
@@ -1,55 +1,76 @@
 import { useState, useEffect } from 'react';
 import { Link } from 'react-router-dom';
-import { AvatarImg } from '../assets';
+import { getUserInfo } from '../utils';
 import { useMenuItem } from '../contexts';
 import { PageRoutes } from '../constants';
 import { Logout } from '../layouts';
 import { IUser, IFollowing, IPlaylists } from '../interfaces';
-import { 
-  Main, Header, Avatar, Username, 
-  Stats, Stat, Number, NumLabel 
+import {
+  Main, Header, Avatar, Username,
+  Stats, Stat, Number, NumLabel
 } from '../components';
 
 function User() {
   const { setActiveItem } = useMenuItem();
-  const [user, setUser] = useState<IUser | null>(null);  
+  const [user, setUser] = useState<IUser | null>(null);
   const [following, setFollowing] = useState<IFollowing | null>(null);
   const [playlists, setPlaylists] = useState<IPlaylists | null>(null);
 
   useEffect(() => {
     setActiveItem(1);
+    getData();
   }, []);
 
+  const getData = async () => {
+    const { user, following, playlists } = await getUserInfo();
+    setUser(user);
+    setFollowing(following);
+    setPlaylists(playlists);
+  }
+
   return (
-    <Main>
-      <Header>
-        <Avatar src={AvatarImg} alt='avatar' />
-        <Username
-          href='#'
-          target='_blank'
-          rel='noopener noreferrer'
-        >
-          Felix
-        </Username>
-        <Stats>
-          <Stat>
-            <Number>0</Number>
-            <NumLabel>followers</NumLabel>
-          </Stat>
-          <Stat>
-            <Number>3</Number>
-            <NumLabel>following</NumLabel>
-          </Stat>
-          <Stat>
-            <Link to={PageRoutes.PLAYLISTS}>
-              <Number>2</Number>
-              <NumLabel>playlists</NumLabel>
-            </Link>
-          </Stat>
-        </Stats>
-        <Logout />
-      </Header>
-    </Main>
+    <>
+      {user ? (
+        <Main>
+          <Header>
+            {user.images.length > 0 ? (
+              <Avatar 
+                src={user.images[1].url} 
+                alt={`${user.display_name}'s avatar`} 
+              />
+            ) : (
+              <div>No Avatar</div>
+            )}
+            <Username
+              href={user.external_urls.spotify}
+              target='_blank'
+              rel='noopener noreferrer'
+            >
+              {user.display_name}
+            </Username>
+            <Stats>
+              <Stat>
+                <Number>{user.followers.total}</Number>
+                <NumLabel>followers</NumLabel>
+              </Stat>
+              <Stat>
+                <Number>{following?.artists.total}</Number>
+                <NumLabel>following</NumLabel>
+              </Stat>
+              <Stat>
+                <Link to={PageRoutes.PLAYLISTS}>
+                  <Number>{playlists?.total}</Number>
+                  <NumLabel>playlists</NumLabel>
+                </Link>
+              </Stat>
+            </Stats>
+            <Logout />
+          </Header>
+        </Main>
+      ) : (
+        <div>Loading</div>
+      )}
+    </>
   )
 }
 
