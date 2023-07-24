51b3ddc38e84fb46f1ea376ca5fff2c6f4d9c1cb
Giga Amiridze <gigaamiridze@gmail.com>
2023-07-24 09:56:17 +0400

    Added bar chart responsive design

--- src/components/track/Chart.ts
+++ src/components/track/Chart.ts
@@ -4,6 +4,22 @@ export const ChartContainer = styled.div`
   max-width: 700px;
   width: 100%;
   height: 650px;
+  
+  @media (max-width: 800px) {
+    height: 550px;
+  }
+
+  @media (max-width: 600px) {
+    height: 470px;
+  }
+  
+  @media (max-width: 500px) {
+    height: 400px;
+  }
+  
+  @media (max-width: 320px) {
+    height: 380px;
+  }
 
   #chart {
     width: 100% !important;
