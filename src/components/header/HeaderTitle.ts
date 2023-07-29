336bf2f25b74e664b65783f3c9a49cf8cc5ad1c2
Giga Amiridze <gigaamiridze@gmail.com>
2023-07-29 20:33:13 +0400

    Added main title for recommendations page

--- src/components/header/HeaderTitle.ts
+++ src/components/header/HeaderTitle.ts
@@ -4,6 +4,15 @@ const HeaderTitle = styled.h2`
   color: ${({ theme }) => theme.colors.white};
   font-size: clamp(22px, 5.8vw, 24px);
   font-weight: 900;
+  line-height: 1.2;
+  
+  a {
+    transition: ${({ theme }) => theme.transition};
+    
+    &:hover {
+      color: ${({ theme }) => theme.colors.lightGreen};
+    }
+  }
   
   @media (max-width: 768px) {
     text-align: center;
--- src/pages/Recommendations.tsx
+++ src/pages/Recommendations.tsx
@@ -1,13 +1,47 @@
-import { useParams } from 'react-router-dom';
-import { Main } from '../components';
+import {useEffect, useState} from 'react';
+import { useParams, Link } from 'react-router-dom';
+import { Loader } from '../layouts';
+import { getPlaylist } from '../utils';
+import { PageRoutes } from '../constants';
+import { IPlaylist } from '../interfaces';
+import { pageAnimation } from '../animations';
+import { PageContainer, HeaderTitle } from '../components';
 
 function Recommendations() {
   const { playlistId } = useParams();
+  const [playlist, setPlaylist] = useState<IPlaylist | null>(null);
+
+  useEffect(() => {
+    getPlaylistInfo();
+  }, [playlistId]);
+
+  const getPlaylistInfo = async () => {
+    if (playlistId) {
+      const { data } = await getPlaylist(playlistId);
+      setPlaylist(data);
+    }
+  }
 
   return (
-    <Main>
-      Playlist ID: {playlistId}
-    </Main>
+    <>
+      {playlist ? (
+        <PageContainer
+          variants={pageAnimation}
+          initial='initial'
+          animate='animate'
+          exit='exit'
+        >
+          <HeaderTitle>
+            Recommended Tracks Based On&nbsp;
+            <Link to={`${PageRoutes.HOST}/${PageRoutes.PLAYLIST}/${playlist.id}`}>
+              {playlist.name}
+            </Link>
+          </HeaderTitle>
+        </PageContainer>
+      ) : (
+        <Loader />
+      )}
+    </>
   )
 }
 
