48ef01bbcd5132d8ad33ddcba3a76683856827f4
Giga Amiridze <gigaamiridze@gmail.com>
2023-07-30 17:29:03 +0400

    Added responsive design for recommendations header

--- src/components/recommendations/RecommendationsHeader.ts
+++ src/components/recommendations/RecommendationsHeader.ts
@@ -4,7 +4,12 @@ const RecommendationsHeader = styled.div`
   display: flex;
   align-items: center;
   justify-content: space-between;
-  column-gap: 50px;
+  gap: 50px;
+  
+  @media (max-width: 768px) {
+    flex-direction: column;
+    gap: 20px;
+  }
 `;
 
 export default RecommendationsHeader;
